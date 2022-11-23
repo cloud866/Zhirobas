53ef22a08552f020fbd8f0810f8d909e2478f2be
cloud866 <dklein927@gmail.com>
2022-11-23 19:22:59 +0200

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double weight = Double.parseDouble(br.readLine());
+        double height = Double.parseDouble(br.readLine());
+        Body.massIndex(weight, height);
+    }
+
+    public static class Body {
+        public static void massIndex(double weight, double height) {
+            double massIndex = weight * height;
+            if (massIndex<18.5){
+                System.out.println("Недовес! поднажми на питание!");
+            }
+            if (massIndex>=18.5 && massIndex<=24.9){
+                System.out.println("Нормальный индекс! Так держать");
+            }
+            if (massIndex>=25 && massIndex<=29.9){
+                System.out.println("Ну ты и жиробас");
+            }
+            if (massIndex>=30){
+                System.out.println("Ну нельзя же так, зашей рот)");
+            }
+        }
+    }
+}
+
